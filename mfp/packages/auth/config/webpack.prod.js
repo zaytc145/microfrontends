1d9a6f5ef1991b4b365ffb8d447644f3f2ca9c80
zaytc145 <zaytsevdanil@yandex.ru>
2022-12-18 17:16:34 +0300

    auth

---
+++ mfp/packages/auth/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJSON = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+    mode: "production",
+    output: {
+        filename: "[name].[contenthash].js",
+        publicPath: "/auth/latest/"
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "auth",
+            filename: "remoteEntry.js",
+            exposes: {
+                "./AuthApp": "./src/bootstrap"
+            },
+            shared: packageJSON.dependencies
+        })
+    ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
